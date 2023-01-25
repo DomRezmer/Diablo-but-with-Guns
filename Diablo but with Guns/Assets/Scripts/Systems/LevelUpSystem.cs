67821324d7385f16d1a429dad44a3ace1e07d8dd
Schaggy <84445733+DuRant1337@users.noreply.github.com>
2023-01-25 20:54:30 +0100

    XP Bar
    xp added

--- Diablo but with Guns/Assets/Scripts/Systems/LevelUpSystem.cs
+++ Diablo but with Guns/Assets/Scripts/Systems/LevelUpSystem.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUpSystem : MonoBehaviour
 {
     public int currentLevel;
     public int baseEXP = 20;
     public int currentEXP;
+    public Text lvlText;
+    public Image expBarImage;
 
     public int expForNextLevel;
     public int expDifferenceToNextLevel;
@@ -24,6 +27,15 @@ public class LevelUpSystem : MonoBehaviour
         //InvokeRepeating("AddEXP", 1f, 1f);
     }
 
+    public static int ExpNeedToLvlUp(int currentLevel)
+    {
+        if (currentLevel == 0)
+        {
+            return 0;
+        }
+        return (currentLevel * currentLevel + currentLevel) + 5;
+    }
+
     public void AddEXP()
     {
         CalculateLevel(25);
@@ -36,21 +48,31 @@ public class LevelUpSystem : MonoBehaviour
 
     void CalculateLevel(int amount)
     {
+        float previousExp = ExpNeedToLvlUp(currentLevel - 1);
+
         currentEXP += amount;
 
-        int tmp_cur_level = (int)Mathf.Sqrt(currentEXP / baseEXP)+1;
-
-        if(currentLevel != tmp_cur_level)
+        int tmp_cur_level = (int)Mathf.Sqrt(currentEXP / baseEXP) + 1;
+        if (currentLevel != tmp_cur_level)
         {
-            currentLevel= tmp_cur_level;
+            currentLevel = tmp_cur_level;
+            lvlText.text = currentLevel.ToString("");
+            previousExp = ExpNeedToLvlUp(currentLevel - 1);
         }
 
         expForNextLevel = baseEXP * currentLevel * currentLevel;
         expDifferenceToNextLevel = expForNextLevel - currentEXP;
-        totalEXPDifference = expForNextLevel - (baseEXP * (currentLevel-1) * (currentLevel-1));
+        totalEXPDifference = expForNextLevel - (baseEXP * (currentLevel - 1) * (currentLevel - 1));
 
         fillAmount = (float)expDifferenceToNextLevel / (float)totalEXPDifference;
         reversedFillAmount = 1 - fillAmount;
+
+        expBarImage.fillAmount = (currentEXP - previousExp) / (expForNextLevel - previousExp);
+
+        if (expBarImage.fillAmount == 1)
+        {
+            expBarImage.fillAmount = 0;
+        }
 
         statPoints = 5 * (currentLevel - 1);
         skillPoints = 15 * (currentLevel - 1);
